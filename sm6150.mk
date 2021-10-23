b607263b04a246a3e273a28ca3122324920c39bc
TyraLizQ10 <nathanielquitua@gmail.com>
2021-10-23 13:19:31 +0800

    sm6150-common: remove packages on build time

--- sm6150.mk
+++ sm6150.mk
@@ -111,9 +111,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@2.0.vendor \
     vendor.qti.hardware.camera.device@3.5.vendor
 
-PRODUCT_PACKAGES += \
-    Snap
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
@@ -337,6 +334,10 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
 
 # RIL
 PRODUCT_PACKAGES += \
